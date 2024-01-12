8688e71e2324a93a5fe2c5246242086c00c6c3a3
vectorzhao <1092381273@qq.com>
2024-01-12 21:21:45 +0800

    优化docker log输出

--- scan_plex_tg_channel.py
+++ scan_plex_tg_channel.py
@@ -5,10 +5,6 @@ import schedule
 import asyncio
 import datetime
 
-
-# 环境变量中读取的定时扫描时间（格式为 HH:MM）
-scan_time = os.getenv('SCAN_TIME', '')  # 默认为空
-
 # 从环境变量读取配置
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
@@ -18,6 +14,7 @@ plex_token = os.getenv('PLEX_TOKEN')
 plex_url = os.getenv('PLEX_URL')
 libraries_config = os.getenv('LIBRARIES_CONFIG', '')  # 默认为空字符串
 channel_username = os.getenv('CHANNEL_USERNAME', 'umziebook')  # 默认频道名
+scan_time = os.getenv('SCAN_TIME', '')  # 定时扫描时间
 
 # 解析媒体库配置
 libraries = {}
@@ -25,10 +22,6 @@ if libraries_config:
     for lib in libraries_config.split(','):
         lib_id, lib_name = lib.split(':')
         libraries[lib_id.strip()] = lib_name.strip()
-
-def scheduled_scan():
-    print(f"定时扫描启动 - {datetime.datetime.now()}")
-    scan_plex_libraries(plex_url, plex_token, libraries)
 
 def send_telegram_message(bot_token, chat_id, message):
     send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
@@ -46,13 +39,24 @@ def scan_plex_libraries(plex_url, plex_token, libraries):
         else:
             print(f"Plex 媒体库 '{library_name}' 扫描失败，错误码：{response.status_code}")
 
+def scheduled_scan():
+    print(f"定时扫描启动 - {datetime.datetime.now()}")
+    scan_plex_libraries(plex_url, plex_token, libraries)
+
 # 定时任务设置
 if scan_time:
     schedule.every().day.at(scan_time).do(scheduled_scan)
+    print(f"定时任务已开启：{scan_time}")
+else:
+    print("定时任务未开启")
 
 # 使用 Bot Token 启动客户端
 client = TelegramClient('session_name', api_id, api_hash)
 client.start(bot_token=bot_token)
+
+print("Telegram 已连接成功")
+print(f"监控频道为 {channel_username}")
+print(f"Plex 扫描目录为 {', '.join(libraries.values())}")
 
 async def run_scheduled_tasks():
     while True:
@@ -60,8 +64,6 @@ async def run_scheduled_tasks():
         await asyncio.sleep(1)
 
 client.loop.create_task(run_scheduled_tasks())
-
-print(f"成功连接到 Telegram。开始监测频道：{channel_username}")
 
 @client.on(events.NewMessage(chats=channel_username))
 async def new_message_listener(event):
